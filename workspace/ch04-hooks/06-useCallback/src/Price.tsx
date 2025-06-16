4b832a1dae96af90953b2623edc38dd8db6cfaad
twkimfe <arthurkim135@naver.com>
2025-06-16 11:18:34 +0900

    🚧 Update: ch04 code update
    - ch04/05, 06, 07 수업 내용 학습

---
+++ workspace/ch04-hooks/06-useCallback/src/Price.tsx
@@ -0,0 +1,40 @@
+interface PriceProps {
+  price: number;
+  quantity: number;
+  buyQuantity: number;
+  maxQuantity: number;
+  shippingFees: number;
+  productPrice: number;
+  handleQuantityChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+function Price({
+  price,
+  quantity,
+  maxQuantity,
+  shippingFees,
+  productPrice,
+  handleQuantityChange,
+}: PriceProps) {
+  return (
+    <>
+      <h2>수량 선택</h2>
+      <div>
+        가격: {price.toLocaleString()}원<br />
+        수량:{" "}
+        <input
+          type="number"
+          min="1"
+          max={maxQuantity}
+          value={quantity}
+          onChange={handleQuantityChange}
+        />
+        (배송비는 5개당 {shippingFees.toLocaleString()}원씩 추가됩니다.)
+        <br />
+        상품 금액: {productPrice.toLocaleString()}원
+      </div>
+    </>
+  );
+}
+
+export default Price;
