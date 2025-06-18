084cab110f99450ec30499f23838f05b751150ec
twkimfe <arthurkim135@naver.com>
2025-06-18 16:50:40 +0900

    🚧 Update: ch05 code update
    - React ch05 학습

--- workspace/ch12-app/todolist/05-router/src/App.tsx
+++ workspace/ch12-app/todolist/05-router/src/App.tsx
@@ -1,8 +1,10 @@
+import { RouterProvider } from "react-router";
+import router from "./routes";
+import './App.css';
+
 function App() {
   return (
-    <>
-      
-    </>
+    <RouterProvider router={ router } />
   );  
 }
 
